e0bbe27a8ee1e452576ed14bdd2efdf633ad8a77
afm-openrecruitment <146961042+afm-openrecruitment@users.noreply.github.com>
2023-10-04 23:44:57 +0800

    Create popup.js

---
+++ popup.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+    $('.popup-image').magnificPopup({
+        type: 'image',
+        closeOnContentClick: true,
+        closeBtnInside: false,
+        fixedContentPos: true,
+        mainClass: 'mfp-no-margins mfp-with-zoom',
+        image: {
+            verticalFit: true
+        },
+        zoom: {
+            enabled: true,
+            duration: 300
+        }
+    });
+});
