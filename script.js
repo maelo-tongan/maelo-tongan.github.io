461d8f08f92e682b5bef11cae802bbd6d9899d92
afm-openrecruitment <146961042+afm-openrecruitment@users.noreply.github.com>
2023-10-04 21:54:32 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+    $('form').submit(function(event) {
+        event.preventDefault(); // Mencegah form untuk melakukan submit
+
+        // Mengambil nilai dari input form
+        var nama = $('#nama').val();
+        var alamat = $('#alamat').val();
+        var telepon = $('#telepon').val();
+        var pendidikan = $('#pendidikan').val();
+        var pengalaman = $('#pengalaman').val();
+
+        // Format pesan WhatsApp
+        var message = 'Halo, saya ingin mendaftar dengan data sebagai berikut:\n\n' +
+            'Nama Lengkap: ' + nama + '\n' +
+            'Alamat Lengkap: ' + alamat + '\n' +
+            'Nomor Telepon: ' + telepon + '\n' +
+            'Pendidikan Terakhir: ' + pendidikan + '\n' +
+            'Pengalaman Kerja (jika ada): ' + pengalaman;
+
+        // Mengirim pesan WhatsApp
+        window.open('https://web.whatsapp.com/send/?phone=6285299747503&text=' + encodeURIComponent(message));
+    });
+});
